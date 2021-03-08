69054db8154334ba15fc304d1ee2d8c9a5d09ea9
Akshay M <akshay.milmile@gmail.com>
2021-03-08 19:51:15 +0100

    undo formatting

--- screens/Article.js
+++ screens/Article.js
@@ -1,40 +1,30 @@
-import React, { Component } from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  Animated,
-  Image,
-  Dimensions,
-  ScrollView,
-  TouchableOpacity,
-} from "react-native";
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Animated, Image, Dimensions, ScrollView, TouchableOpacity } from 'react-native'
 
-import FontAwesome from "react-native-vector-icons/FontAwesome";
-import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-import * as theme from "../theme";
-import { getDestinationStatus } from "../utils/api";
+import * as theme from '../theme';
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   flex: {
     flex: 0,
   },
   column: {
-    flexDirection: "column",
+    flexDirection: 'column'
   },
   row: {
-    flexDirection: "row",
+    flexDirection: 'row'
   },
   header: {
     // backgroundColor: 'transparent',
     paddingHorizontal: theme.sizes.padding,
     paddingTop: theme.sizes.padding,
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "absolute",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
@@ -42,8 +32,8 @@ const styles = StyleSheet.create({
   back: {
     width: theme.sizes.base * 3,
     height: theme.sizes.base * 3,
-    justifyContent: "center",
-    alignItems: "flex-start",
+    justifyContent: 'center',
+    alignItems: 'flex-start',
   },
   content: {
     // backgroundColor: theme.colors.active,
@@ -51,7 +41,7 @@ const styles = StyleSheet.create({
     // borderTopRightRadius: theme.sizes.border,
   },
   contentHeader: {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     padding: theme.sizes.padding,
     backgroundColor: theme.colors.white,
     borderTopLeftRadius: theme.sizes.radius,
@@ -59,7 +49,7 @@ const styles = StyleSheet.create({
     marginTop: -theme.sizes.padding / 2,
   },
   avatar: {
-    position: "absolute",
+    position: 'absolute',
     top: -theme.sizes.margin,
     right: theme.sizes.margin,
     width: theme.sizes.padding * 2,
@@ -76,12 +66,12 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
   },
   dotsContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
     bottom: 36,
     right: 0,
-    left: 0,
+    left: 0
   },
   dots: {
     width: 8,
@@ -92,13 +82,13 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: theme.sizes.font * 2,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   description: {
     fontSize: theme.sizes.font * 1.2,
     lineHeight: theme.sizes.font * 2,
-    color: theme.colors.caption,
-  },
+    color: theme.colors.caption
+  }
 });
 
 class Article extends Component {
@@ -109,68 +99,59 @@ class Article extends Component {
     return {
       header: (
         <View style={[styles.flex, styles.row, styles.header]}>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.goBack()}
-          >
-            <FontAwesome
-              name="chevron-left"
-              color={theme.colors.white}
-              size={theme.sizes.font * 1}
-            />
+          <TouchableOpacity style={styles.back} onPress={() => navigation.goBack()}>
+            <FontAwesome name="chevron-left" color={theme.colors.white} size={theme.sizes.font * 1} />
           </TouchableOpacity>
           <TouchableOpacity>
-            <MaterialIcons
-              name="more-horiz"
-              color={theme.colors.white}
-              size={theme.sizes.font * 1.5}
-            />
+            <MaterialIcons name="more-horiz" color={theme.colors.white} size={theme.sizes.font * 1.5} />
           </TouchableOpacity>
         </View>
       ),
       headerTransparent: true,
-    };
-  };
+    }
+  }
 
   renderDots = () => {
     const { navigation } = this.props;
-    const article = navigation.getParam("article");
+    const article = navigation.getParam('article');
     const dotPosition = Animated.divide(this.scrollX, width);
 
     return (
-      <View style={[styles.flex, styles.row, styles.dotsContainer]}>
+      <View style={[ styles.flex, styles.row, styles.dotsContainer ]}>
         {article.images.map((item, index) => {
           const opacity = dotPosition.interpolate({
             inputRange: [index - 1, index, index + 1],
             outputRange: [0.5, 1, 0.5],
-            extrapolate: "clamp",
+            extrapolate: 'clamp'
           });
           return (
             <Animated.View
               key={`step-${item}-${index}`}
               style={[styles.dots, { opacity }]}
             />
-          );
+          )
         })}
       </View>
-    );
-  };
+    )
+  }
 
   renderRatings = (rating) => {
     const stars = new Array(5).fill(0);
-    return stars.map((_, index) => {
-      const activeStar = Math.floor(rating) >= index + 1;
-      return (
-        <FontAwesome
-          name="star"
-          key={`star-${index}`}
-          size={theme.sizes.font}
-          color={theme.colors[activeStar ? "active" : "gray"]}
-          style={{ marginRight: 4 }}
-        />
-      );
-    });
-  };
+    return (
+      stars.map((_, index) => {
+        const activeStar = Math.floor(rating) >= (index + 1);
+        return (
+          <FontAwesome
+            name="star"
+            key={`star-${index}`}
+            size={theme.sizes.font}
+            color={theme.colors[activeStar ? 'active' : 'gray']}
+            style={{ marginRight: 4 }}
+          />
+        )
+      })
+    )
+  }
 
   async componentDidMount() {
     const { navigation } = this.props;
@@ -185,8 +166,8 @@ class Article extends Component {
 
   render() {
     const { navigation } = this.props;
-    const article = navigation.getParam("article");
-    console.log("availability", this.state.available);
+    const article = navigation.getParam('article');
+
     return (
       <View style={styles.flex}>
         <View style={[styles.flex]}>
@@ -198,43 +179,35 @@ class Article extends Component {
             decelerationRate={0}
             scrollEventThrottle={16}
             snapToAlignment="center"
-            onScroll={Animated.event([
-              { nativeEvent: { contentOffset: { x: this.scrollX } } },
-            ])}
+            onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: this.scrollX } } }])}
           >
-            {article.images.map((img, index) => (
-              <Image
-                key={`${index}-${img}`}
-                source={{ uri: img }}
-                resizeMode="cover"
-                style={{ width, height: width }}
-              />
-            ))}
+            {
+              article.images.map((img, index) => 
+                <Image
+                  key={`${index}-${img}`}
+                  source={{ uri: img }}
+                  resizeMode='cover'
+                  style={{ width, height: width }}
+                />
+              )
+            }
           </ScrollView>
           {this.renderDots()}
         </View>
         <View style={[styles.flex, styles.content]}>
           <View style={[styles.flex, styles.contentHeader]}>
-            <Image
-              style={[styles.avatar, styles.shadow]}
-              source={{ uri: article.user.avatar }}
-            />
+            <Image style={[styles.avatar, styles.shadow]} source={{ uri: article.user.avatar }} />
             <Text style={styles.title}>{article.title}</Text>
-            <View
-              style={[
-                styles.row,
-                {
-                  alignItems: "center",
-                  marginVertical: theme.sizes.margin / 2,
-                },
-              ]}
-            >
+            <View style={[
+              styles.row,
+              { alignItems: 'center', marginVertical: theme.sizes.margin / 2 }
+            ]}>
               {this.renderRatings(article.rating)}
               <Text style={{ color: theme.colors.active }}>
-                {article.rating}
+                {article.rating} 
               </Text>
               <Text style={{ marginLeft: 8, color: theme.colors.caption }}>
-                ({article.reviews} reviews)
+                 ({article.reviews} reviews)
               </Text>
             </View>
             <Text style={{ color: theme.colors.active }}>
@@ -245,14 +218,14 @@ class Article extends Component {
             </Text>
             <TouchableOpacity>
               <Text style={styles.description}>
-                {article.description.split("").slice(0, 180)}...
-                <Text style={{ color: theme.colors.active }}> Read more</Text>
+                {article.description.split('').slice(0, 180)}...
+                <Text style={{color: theme.colors.active}}> Read more</Text>
               </Text>
             </TouchableOpacity>
           </View>
         </View>
       </View>
-    );
+    )
   }
 }
 
